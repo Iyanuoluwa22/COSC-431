92280ae63e0ec6515ead042c797061c29b63e0ce
Iyanuoluwa Ogunfiditimi <iyanu.mehlek22@gmail.com>
2024-07-10 15:28:48 -0400

    Added personal branch

--- webpack.config.js
+++ webpack.config.js
@@ -29,5 +29,14 @@ module.exports = {
     port: 3000,
     open: true,
     hot: true
+  },
+  resolve: {
+    fallback: {
+      "http": require.resolve("stream-http"),
+      "https": require.resolve("https-browserify"),
+      "url": require.resolve("url/"),
+      "buffer": require.resolve("buffer/"),
+      "stream": require.resolve("stream-browserify")
+    }
   }
 };
