aa7314bb3facc735fc8fe4dfcb095f6172b8e622
kreid2003 <160911673+kreid2003@users.noreply.github.com>
2024-07-15 13:07:27 -0400

    Update Content.js
    added the url to the four celebs and added their four latest transactions respectively. Transaction info includes the transaction hash, from hash, to hash, and how much ETH was in the transaction.

--- src/components/Content.js
+++ src/components/Content.js
@@ -9,23 +9,46 @@ const Content = () => {
         <div className="latest-block">
           <h2>CELEB PROFILES</h2>
           <ul>
-            <li> <a href="url">Snoop Dog</a>
+            <li> <a href="https://etherscan.io/address/0xCe90a7949bb78892F159F428D0dC23a8E3584d75">Snoop Dog</a>
         </li>
-            <li> <a href="url">Mark Cuban</a>
+            <li> <a href="https://etherscan.io/address/0xa679c6154b8d4619Af9F83f0bF9a13A680e01eCf">Mark Cuban</a>
         </li>
-            <li> <a href="url">Post Malone</a>
+            <li> <a href="https://etherscan.io/address/0xbEA020c3bD417f30De4d6bd05b0ED310ac586cc0">Post Malone</a>
         </li>
-            <li> <a href="url">Paris Hilton</a>
+            <li> <a href="https://etherscan.io/address/0xb6aa5a1aa37a4195725cdf1576dc741d359b56bd">Paris Hilton</a>
         </li>
           </ul>
         </div>
         <div className="latest-transaction">
           <h2>LATEST TRANSACTION</h2>
           <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li>
+            <p>Transaction ID:<a href="https://etherscan.io/tx/0xa2eed55dd5412889dcb32ccbf6d26bbdf6134dc19d2d74fb8c1ee7eadbf6f7d8">0xa2eed55d...f7d8</a> </p>
+            <p>From:<a href="https://etherscan.io/address/0xce6fac475851dd566626e0f27ea661cfdca0f9f1">0xce6Fa...0f9F1</a> </p>
+            <p>To:<a href="https://etherscan.io/address/0xCe90a7949bb78892F159F428D0dC23a8E3584d75">0xCe9...4d75</a> </p>
+            <p>Amount: 0.000526 ETH</p>
+            </li>
+            
+            <li>
+            <p>Transaction ID:<a href="https://etherscan.io/tx/0x6ad4b782bde6ead80b020161133bee676e9d40856d1de5b75e64746cb1c80717">0x6ad4b7...80717</a> </p>
+            <p>From:<a href="https://etherscan.io/address/0xa679c6154b8d4619Af9F83f0bF9a13A680e01eCf">0xa679c...1eCf</a> </p>
+            <p>To:<a href="https://etherscan.io/address/0x733ea491051929d0b6619425e088d210deb5cbe6">0x733EA...cBE6</a> </p>
+            <p>Amount:0.888735157089728196 ETH </p>
+            </li>
+
+            <li>
+            <p>Transaction ID:<a href="https://etherscan.io/tx/0x3879a92f2628be3928a98b130724e71c454aa2705fd3dfd07700d03000a70ab9">0x3879...0ab9</a> </p>
+            <p>From:<a href="https://etherscan.io/address/0xada412f7c7acfd52d1f8adad8715afc672fd9166">0xadA412f7C7...9166</a> </p>
+            <p>To:<a href="https://etherscan.io/address/0xbEA020c3bD417f30De4d6bd05b0ED310ac586cc0">0xbEA020c3bD417...cc0</a> </p>
+            <p>Amount:0.000101 ETH </p>
+            </li>
+
+            <li>
+            <p>Transaction ID:<a href="https://etherscan.io/tx/0xce37d8b93226e81a938af4c0a33fccd6b7965dbe938041e6c6286e575d66e466">0xce37d8...6e466</a> </p>
+            <p>From:<a href="https://etherscan.io/address/0x5aaffa92f8567cffe4688e878e49359779fbe86b">0x5aAfF...be86b</a> </p>
+            <p>To:<a href="https://etherscan.io/address/0xb6aa5a1aa37a4195725cdf1576dc741d359b56bd">0xB6Aa5a...56bd</a> </p>
+            <p>Amount:0.00000606 ETH </p>
+            </li>
           </ul>
         </div>
       </div>
@@ -33,4 +56,5 @@ const Content = () => {
   );
 };
 
+export default Content;
 export default Content;
