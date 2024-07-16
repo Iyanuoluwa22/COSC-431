238552e2524cff3ddc03cdef0114ec73e632a0e9
WaitingPhoFood <93995409+WaitingPhoFood@users.noreply.github.com>
2024-07-15 21:53:04 -0400

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,24 @@
 // src/components/Header.js
 import React from 'react';
+
 import { Link } from 'react-router-dom';
+import { signOut } from 'firebase/auth';
+import { auth } from '../firebase';
 import './Header.css';
 
-const Header = ({ onLogoClick, onNavClick, isLoggedIn }) => {
-  return (
+const Header = ({ onLogoClick, onNavClick, isLoggedIn, onLogout }) => {
+ 
+
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth);
+      onLogout(); 
+    } catch (error) {
+      console.error('Error signing out: ', error);
+    }
+  };
+
+ return (
     <header>
       <div className="logo" onClick={onLogoClick} style={{ cursor: 'pointer' }}>
         <h1 className="logo-text"><span>CELEB</span>SCAN</h1>
@@ -13,14 +27,23 @@ const Header = ({ onLogoClick, onNavClick, isLoggedIn }) => {
       <ul className="nav">
         <li><Link to="/" onClick={onNavClick}>Home</Link></li>
         <li><Link to="/profile">Profile</Link></li>
-        
-        <li>
-          <Link to="/login">
-            <i className="fa fa-user"></i>
-            Login/Register
-          </Link>
-        </li>
-        {isLoggedIn && <li><Link to="/chat" onClick={onNavClick}>Chat</Link></li>} {/* Conditionally render Chat link */}
+
+      
+        {isLoggedIn ? (
+          <>
+            <li><Link to="/chat" onClick={onNavClick}>Chat</Link></li>
+            <li><button className="sign-out-btn" onClick={handleSignOut}>Sign Out</button></li>
+          </>
+        ) : (
+
+
+          <li>
+            <Link to="/login">
+              <i className="fa fa-user"></i>
+              Login/Register
+            </Link>
+          </li>
+        )}
       </ul>
     </header>
   );
